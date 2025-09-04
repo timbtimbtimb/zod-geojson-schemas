312c95f37808da0a60ec219fb60e595bb679bcc3
Timothée Billiet Cadart <timothee.billiet@arturia.com>
2025-09-04 09:58:33 +0200

    🐛 Export types from index

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
-import { FeatureSchema } from "./Feature";
-import { FeatureCollectionSchema } from "./FeatureCollection";
-import { GeometrySchema } from "./Geometry";
-import { GeometryCollectionSchema } from "./GeometryCollection";
-import { LineStringSchema } from "./LineString";
-import { MultiLineStringSchema } from "./MultiLineString";
-import { MultiPointSchema } from "./MultiPoint";
-import { MultiPolygonSchema } from "./MultiPolygon";
-import { PointSchema } from "./Point";
-import { PolygonSchema } from "./Polygon";
-import { PositionSchema } from "./Position";
-import { PropertiesSchema } from "./Properties";
-import { BBoxSchema } from "./BBox";
+import { type Feature, FeatureSchema } from "./Feature";
+import { type FeatureCollection, FeatureCollectionSchema } from "./FeatureCollection";
+import { type Geometry, GeometrySchema } from "./Geometry";
+import { type GeometryCollection, GeometryCollectionSchema } from "./GeometryCollection";
+import { type LineString, LineStringSchema } from "./LineString";
+import { type MultiLineString, MultiLineStringSchema } from "./MultiLineString";
+import { type MultiPoint, MultiPointSchema } from "./MultiPoint";
+import { type MultiPolygon, MultiPolygonSchema } from "./MultiPolygon";
+import { type Point, PointSchema } from "./Point";
+import { type Polygon, PolygonSchema } from "./Polygon";
+import { type Position, PositionSchema } from "./Position";
+import { type Properties, PropertiesSchema } from "./Properties";
+import { type BBox, BBoxSchema } from "./BBox";
 
 export {
   FeatureSchema,
@@ -25,5 +25,18 @@ export {
   PolygonSchema,
   PositionSchema,
   PropertiesSchema,
-  BBoxSchema
+  BBoxSchema,
+  Feature,
+  FeatureCollection,
+  Geometry,
+  GeometryCollection,
+  LineString,
+  MultiLineString,
+  MultiPoint,
+  MultiPolygon,
+  Point,
+  Polygon,
+  Position,
+  Properties,
+  BBox
 }
